286a87e4d0d2ab89b8c893f29aff3d92e79f5a81
ChegeDaniella <chegedaniella@gmail.com>
2020-04-02 21:28:37 +0300

    highlight one quote is working

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -4,18 +4,13 @@ import { Directive, ElementRef,HostListener } from '@angular/core';
   selector: '[appHighlight]'
 })
 export class HighlightDirective {
-  constructor(private elem:ElementRef){}
 
-  @HostListener("click")onClicks(){
-    this.color("grey")
+  constructor(private elem:ElementRef){
+    this.elem.nativeElement.style.backgroundColor='yellow'
   }
-  @HostListener("click")onDoubleClicks(){
-    this.color("None")
-  }
-  private color(action:string){
-    this.elem.nativeElement.style.backgroundColor=action;
-  }
-  
+
 
 
 }
+
+
