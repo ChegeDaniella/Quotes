2d10559d16587207217a059c05d5cad00b90dae8
ChegeDaniella <chegedaniella@gmail.com>
2020-04-02 22:24:21 +0300

    hostlistener on dirctive working

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -5,9 +5,28 @@ import { Directive, ElementRef,HostListener } from '@angular/core';
 })
 export class HighlightDirective {
 
-  constructor(private elem:ElementRef){
-    this.elem.nativeElement.style.backgroundColor='yellow'
-  }
+  constructor(private elem:ElementRef){}
+
+@HostListener("click") onClicks(){
+  this.high("yellow")
+}
+
+@HostListener("dblclick") onDoubleClicks(){
+  this.high("None")
+}
+
+private high(action:string){
+  this.elem.nativeElement.style.backgroundColor=action;
+}
+
+
+
+
+
+
+
+    
+  
 
 
 
