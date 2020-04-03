819fdba0055c8cfa2ccde7f137566413de661ba1
ChegeDaniella <chegedaniella@gmail.com>
2020-04-03 17:18:58 +0300

    date ago pipe working

--- src/app/time-passed.pipe.ts
+++ src/app/time-passed.pipe.ts
@@ -1,24 +1,37 @@
 import { Pipe, PipeTransform } from '@angular/core';
 
 @Pipe({
-  name: 'timePassed'
+  name: 'timePassed',
+  pure: true
 })
 export class TimePassedPipe implements PipeTransform {
 
-  transform(value: any): any {
-    let today : Date = new Date()// get current date
-    let inputDate: any= new Date(today.getFullYear(),today.getMonth(),today.getDate());
-    let diffrenceBtw=Math.abs(inputDate-value);
-    const secondsInDay=86400;
-    let differenceInSeconds=diffrenceBtw*0.001;
-    let countTotal=differenceInSeconds/secondsInDay;
-
-    if(countTotal>=0){
-      return Math.ceil(countTotal)+" "+"days ago.";
-    } else {
-      return 0;
+  transform(value: any, args?: any): any {
+    if (value) {
+        const seconds = Math.floor((+new Date() - +new Date(value)) / 1000);
+        if (seconds < 29) 
+            return 'Just now';
+        const intervals = {
+            'year': 31536000,
+            'month': 2592000,
+            'week': 604800,
+            'day': 86400,
+            'hour': 3600,
+            'minute': 60,
+            'second': 1
+        };
+        let counter;
+        for (const i in intervals) {
+            counter = Math.floor(seconds / intervals[i]);
+            if (counter > 0)
+                if (counter === 1) {
+                    return counter + ' ' + i + ' ago'; 
+                } else {
+                    return counter + ' ' + i + 's ago'; 
+                }
+        }
     }
-
-  }
+    return value;
+}
 
 }
